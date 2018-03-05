691af4caddce550718d6d2057c231a7f09e25d66
SHEMA <SHEMA@192.168.0.15>
2018-03-04 18:41:50 -0600

    test

---
+++ src/assignments1/Alphabet.java
@@ -0,0 +1,41 @@
+package assignments1;
+
+import java.util.Scanner;
+
+public class Alphabet {
+		
+//		Scanner input = new Scanner(System.in);
+//		
+//		System.out.println("Input an alphabet: ");
+//		String letter = input.next().toLowerCase(); 
+//		if ((letter.length() > 1) || (!Character.isAlphabetic(letter.charAt(0)))) {
+//		System.out.println("Error! Please enter a letter.");
+//		} 
+//		else if 
+//		((letter.charAt(0) == 'a') || (letter.charAt(0) == 'e') || (letter.charAt(0) == 'i')
+//		|| (letter.charAt(0) == 'o') || (letter.charAt(0) == 'u')) {
+//		System.out.println("Input letter is a Vowel");
+//		} 
+//		else {
+//		System.out.println("Input letter is a Constant");
+//		}
+//		
+		
+	
+		  public static void main(String[] args){
+		     String : for(int i = 0; i< 10; i++){
+		        for (int j = 0; j< 10; j++){
+		             if ( i+ j > 10 )  break String;
+		        }
+		        System.out.println( "hello");
+		     }
+		  }
+		
+		
+
+	}
+
+              	
+        	
+        	
+        

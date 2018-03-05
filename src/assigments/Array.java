9f836a0a7add9a8cd38517488071a24d241e4928
SHEMA <SHEMA@192.168.0.15>
2018-03-04 19:48:17 -0600

    test

---
+++ src/assigments/Array.java
@@ -0,0 +1,61 @@
+package assigments;
+
+public class Array {
+	public static void main(String[] args) {
+		String [] Cuteanimals=new String [25];
+		Cuteanimals[0]="Giraf";
+		Cuteanimals[1]="Dog";
+		Cuteanimals[2]="Sheep";
+		Cuteanimals[3]="Bear";
+		Cuteanimals[4]="Frog";
+		Cuteanimals[5]="Skunk";
+		Cuteanimals[6]="Cuteone";
+		Cuteanimals[7]="Cutetwo";
+		Cuteanimals[8]="Hippo";
+		Cuteanimals[9]="Monkey";
+		Cuteanimals[10]="BrownBear";
+		Cuteanimals[11]="Lion";
+		Cuteanimals[12]="Cat";
+		Cuteanimals[13]="Donkey";
+		Cuteanimals[14]="Mice";
+		Cuteanimals[15]="Panda";
+		Cuteanimals[16]="Chick";
+		Cuteanimals[17]="Elephant";
+		Cuteanimals[18]="Goat";
+		Cuteanimals[19]="Pig";
+		Cuteanimals[20]="Crocodile";
+		Cuteanimals[21]="Rabbit";
+		Cuteanimals[22]="Duck";
+		Cuteanimals[23]="Hedgehog";
+		Cuteanimals[24]="Penguin";
+		
+		System.out.println(Cuteanimals[0]);
+		System.out.println(Cuteanimals[1]);
+		System.out.println(Cuteanimals[2]);
+		System.out.println(Cuteanimals[3]);
+		System.out.println(Cuteanimals[4]);
+		System.out.println(Cuteanimals[5]);
+		System.out.println(Cuteanimals[6]);
+		System.out.println(Cuteanimals[7]);
+		System.out.println(Cuteanimals[8]);
+		System.out.println(Cuteanimals[9]);
+		System.out.println(Cuteanimals[10]);
+		System.out.println(Cuteanimals[11]);
+		System.out.println(Cuteanimals[12]);
+		System.out.println(Cuteanimals[13]);
+		System.out.println(Cuteanimals[14]);
+		System.out.println(Cuteanimals[15]);
+		System.out.println(Cuteanimals[16]);
+		System.out.println(Cuteanimals[17]);
+		System.out.println(Cuteanimals[18]);
+		System.out.println(Cuteanimals[19]);
+		System.out.println(Cuteanimals[20]);
+		System.out.println(Cuteanimals[21]);
+		System.out.println(Cuteanimals[22]);
+		System.out.println(Cuteanimals[23]);
+		System.out.println(Cuteanimals[24]);
+		
+		
+	}
+
+}
